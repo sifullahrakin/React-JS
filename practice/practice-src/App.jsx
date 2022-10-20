c06eff059c146e7ea29e0af91eb422bfbf883382
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2022-10-20 22:40:13 +0600

    1

---
+++ practice/practice-src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./heading.jsx";
+import Food_List from "./list_items.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Food_List />
+    </div>
+  );
+}
+export default App;
